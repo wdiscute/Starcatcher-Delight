af3ee4e6f45423c08c44ded4b11301e0c4242dc8
wd <lucasalvesbispo2@gmail.com>
2026-07-14 16:48:57 +0100

    v3.0.1 publish, starcatcher v3.0.6 bump, wdutils v1.2 bump

--- src/main/java/com/wdiscute/starcatcher_delight/registry/SDCreativeTab.java
+++ src/main/java/com/wdiscute/starcatcher_delight/registry/SDCreativeTab.java
@@ -5,33 +5,26 @@ import net.mcexpanded.fancytabsections.FancyTabSections;
 import net.mcexpanded.fancytabsections.Section.SectionColored;
 import net.mcexpanded.fancytabsections.creativetab.ConglomerateOfItems;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
-
-import java.util.function.Supplier;
 
 public class SDCreativeTab
 {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, StarcatcherDelight.MOD_ID);
 
-    public static void addItems()
+    public static void register(IEventBus bus)
     {
+
+        FancyTabSections.registerCreativeModeTab(bus, StarcatcherDelight.rl("starcatcher_delight"), SDItems.NIGIRI.getUncommon());
 
         //special
         FancyTabSections.addSection(StarcatcherDelight.rl("starcatcher_delight_tab"),
-                new SectionColored(
-                        StarcatcherDelight.rl("special"),
-                        Component.translatable("creativetab.starcatcher_delight.starcatcher_delight.special"),
-                        0xff733838,
-                        0xffffffff,
-                        ConglomerateOfItems.create()
-                                .add(SDItems.SPECIAL_FOODS)
-                )
+                new SectionColored(StarcatcherDelight.rl("special"))
+                        .setTextColor(0xff733838)
+                        .add(SDItems.SPECIAL_FOODS)
         );
 
         //generics
@@ -41,18 +34,14 @@ public class SDCreativeTab
         generic(generics, SDItems.TEMAKI);
         generic(generics, SDItems.URAMAKI);
         generic(generics, SDItems.NIGIRI);
-        //generic(generics, SDItems.HEALTHY_FISH_OMELETTE);
+        generic(generics, SDItems.HEALTHY_FISH_OMELETTE);
         generic(generics, SDItems.FISH_SALAD);
         generic(generics, SDItems.FISH_AND_CHIPS);
 
         FancyTabSections.addSection(StarcatcherDelight.rl("starcatcher_delight_tab"),
-                new SectionColored(
-                        StarcatcherDelight.rl("generic"),
-                        Component.translatable("creativetab.starcatcher_delight.starcatcher_delight.generic"),
-                        0xff733838,
-                        0xffffffff,
-                        generics
-                )
+                new SectionColored(StarcatcherDelight.rl("generic"))
+                        .setTextColor(0xff733838)
+                        .setItems(generics)
         );
     }
 
@@ -61,27 +50,12 @@ public class SDCreativeTab
         for (int i = 0; i < 6; i++)
         {
             con.add(food.get(i));
-            if(i == 5)
+            if (i == 5)
             {
                 con.add(Items.AIR);
                 con.add(Items.AIR);
                 con.add(Items.AIR);
             }
         }
-    }
-
-    public static final Supplier<CreativeModeTab> STARCATCHER_DELIGHT_TAB = CREATIVE_MODE_TAB.register("starcatcher_delight_tab",
-            () -> CreativeModeTab.builder()
-                    .icon(() -> new ItemStack(SDItems.NIGIRI.getUncommon().get()))
-                    .title(Component.translatable("creativetab.starcatcher_delight.starcatcher_delight"))
-                    .displayItems((parameters, output) ->
-                    {
-                        //empty as items are added through FTS
-                    }).build());
-
-    public static void register(IEventBus eventBus)
-    {
-        CREATIVE_MODE_TAB.register(eventBus);
-        addItems();
     }
 }
